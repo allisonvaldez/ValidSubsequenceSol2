0fe3f855808176e054b5b0824670254b5d905f40
Allison Valdez <ajsnipes@usfca.edu>
2021-12-31 15:15:20 -0800

    finished with extra case

--- Mod1/main.py
+++ Mod1/main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# time: O(n) n represents the length of the array | space: O(1)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def test_valid_subsequence(array, sequent_array):
+    # start with creating the sequent starting index
+    sequent_index = 0
+
+    # initialize a for loop to traverse the first array for comparison
+    for i in array:
+        """
+         create a stopping case to prevent the algorithm from traversing out of 
+         bounds of the sequent array by making sure it does not surpass the 
+         length of itself.
+         """
+        if sequent_index == len(sequent_array):
+            break
+
+        """
+        create a statement that checks if the sequence array at a particular 
+        location matches an index in the first array.
+        """
+        if sequent_array[sequent_index] == i:
+            sequent_index += 1
+
+    """
+    Returns true if we found all items in the original array that's equal to the
+    length of the sequent array (all items of the sequent array are found).
+    """
+    return sequent_index == len(sequent_array)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(test_valid_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]))
+print(test_valid_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [26]))
